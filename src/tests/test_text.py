7f0720af8a0eb183dcc4f8f152e7a62df26b624c
Roman Degrik <r.degrik@yandex.ru>
2017-11-29 21:45:38 +0300

    Add magic methods to Text

--- src/tests/test_text.py
+++ src/tests/test_text.py
@@ -7,3 +7,12 @@ def test_text_renders_to_string():
 
 def test_text_renders_to_string_with_colors():
     assert 'test it' == str(Text('test', Green, ' it'))
+
+
+def test_you_can_add_one_text_to_another():
+    assert Text('first') + Text('second') == Text('first', 'second')
+
+
+def test_you_can_add_one_text_to_another_with_colors():
+    assert Text('first') + Text(Green, 'second') == \
+        Text('first', Green, 'second')
--- src/twit_cli/text.py
+++ src/twit_cli/text.py
@@ -1,5 +1,10 @@
+from itertools import chain
+
 class Color:
     name = 'default'
+
+Default = Color
+
 
 class Black(Color):
     name = 'black'
@@ -38,6 +43,15 @@ class TextItem:
         self.text = text
         self.color = color
 
+    def __eq__(self, other):
+        return self.text == other.text and self.color == other.color
+
+    def __repr__(self):
+        return '<{}: {}>'.format(
+            self.color.name,
+            repr(self.text),
+        )
+
 
 class Text:
     def __init__(self, *args):
@@ -55,3 +69,24 @@ class Text:
         for item in self._text:
             result += item.text
         return result
+
+    def __repr__(self):
+        return repr(self._text)
+
+    def __eq__(self, other):
+        return self._text == other._text
+
+    def __add__(self, other):
+        if not isinstance(other, Text):
+            raise TypeError()
+        color = Default
+        result = []
+        for item in chain(self._text, other._text):
+            if item.color != color:
+                color = item.color
+                result.append(color)
+            result.append(item.text)
+        return Text(*result)
+
+    def __iter__(self):
+        return iter(self._text)
