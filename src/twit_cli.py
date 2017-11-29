5583c5fb460dbd411383f82fee78326ef281491c
Roman Degrik <r.degrik@yandex.ru>
2017-11-29 22:26:13 +0300

    Client update.

--- src/twit_cli.py
+++ src/twit_cli.py
@@ -1,19 +1,31 @@
 #!/usr/bin/env python
+import os
+
 import yaml
 
 from twit_cli.api import API
+from twit_cli.ui import CursesUI
 from twit_cli.tweet import Tweet
-from twit_cli.tweet_renderer import TweetRenderer
+from twit_cli.curses_tweet_renderer import CursesTweetRenderer
 
 
 if __name__ == '__main__':
-    credentials = yaml.load(open('creds.yaml'))
+    credentials = yaml.load(open(
+        os.path.join(
+            os.path.dirname(__file__),
+            'creds.yaml',
+        ),
+    ))
     api = API(
         consumer_key=credentials['consumer_key'],
         consumer_secret=credentials['consumer_secret'],
         access_token_key=credentials['access_token_key'],
         access_token_secret=credentials['access_token_secret'],
     )
-    for tw in reversed(api.get_timeline(20)):
-        tweet = Tweet(tw.user.screen_name, tw.text)
-        print(TweetRenderer.render(tweet))
+    with CursesUI() as ui:
+        for tw in reversed(api.get_timeline(20)):
+            tweet = Tweet(tw.user.screen_name, tw.text)
+            ui.print_text(CursesTweetRenderer.render(tweet))
+        for ch in ui.loop():
+            if ord('q') == ch:
+                break
