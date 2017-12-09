0aa27d691f092c4c9129e51600b4a59ccb86606e
Roman Degrik <r.degrik@yandex.ru>
2017-12-09 22:32:23 +0300

    Timeline autoupdate.

--- src/twit_cli.py
+++ src/twit_cli.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 import os
+import curses
+import threading
+import time
 
 import yaml
 
@@ -23,6 +26,25 @@ def redraw(ui, tweets, cursor):
     ui.refresh()
 
 
+def get_timeline(api):
+    return Timeline(
+        list(reversed(list(
+            Tweet(tw.user.screen_name, tw.text)
+            for tw in api.get_timeline(100)
+          )))
+    )
+
+
+def schedule_timeline_update():
+    def autoupdate():
+        while True:
+            time.sleep(10)
+            curses.ungetch(ord('r'))
+    thread = threading.Thread(target=autoupdate, daemon=True)
+    thread.start()
+    return thread
+
+
 if __name__ == '__main__':
     credentials = yaml.load(open(
         os.path.join(
@@ -36,20 +58,21 @@ if __name__ == '__main__':
         access_token_key=credentials['access_token_key'],
         access_token_secret=credentials['access_token_secret'],
     )
-    timeline = Timeline(
-        list(reversed(list(
-            Tweet(tw.user.screen_name, tw.text)
-            for tw in api.get_timeline(100)
-          )))
-    )
+    timeline = get_timeline(api)
+    aupdate = schedule_timeline_update()
     with CursesUI() as ui:
         cursor = Cursor(timeline.tweets, len(timeline.tweets) - 1)
         redraw(ui, timeline.tweets, cursor)
         for ch in ui.loop():
             if ord('q') == ch:
                 break
-            elif ord('j') == ch:
+            elif ch in [ord('j'), curses.KEY_DOWN, 66]:
                 cursor = cursor.down()
-            elif ord('k') == ch:
+            elif ch in [ord('k'), curses.KEY_UP, 65]:
                 cursor = cursor.up()
+            elif ch == ord('r'):
+                timeline = get_timeline(api)
+                cursor = Cursor(timeline.tweets, len(timeline.tweets) - 1)
+            if not aupdate.is_alive():
+                break
             redraw(ui, timeline.tweets, cursor)
